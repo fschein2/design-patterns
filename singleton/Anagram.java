ad28ef5d8733775e50ebcfa506b97b0c12641bf4
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-01 17:28:58 -0400

    completed and documented singleton design pattern

--- singleton/Anagram.java
+++ singleton/Anagram.java
@@ -2,19 +2,37 @@ package singleton;
 
 import java.util.ArrayList;
 
+/**
+ * defines Anagram
+ * @author Fred Schein
+ */
 public class Anagram {
     private String question;
     private ArrayList<String> answers;
     
+    /**
+     * creates an instance of an Anagram
+     * @param question String
+     * @param answers ArrayList<String>
+     */
     public Anagram(String question, ArrayList<String> answers) {
         this.question = question;
         this.answers = answers;
     }
 
+    /**
+     * gets the question of the Anagram
+     * @return question which is a String
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * checks if the user gave a correct response
+     * @param userAnswer String
+     * @return true or false (boolean) depending on if the user was correct or not
+     */
     public boolean isCorrect(String userAnswer) {
         for (int i = 0; i < answers.size(); i++) {
             if (userAnswer.equalsIgnoreCase(answers.get(i))) {
--- singleton/Game.java
+++ singleton/Game.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * defines Game
+ * @author Fred Schein
+ */
 public class Game {
     private static Game game;
     private Random rand;
@@ -12,28 +16,69 @@ public class Game {
     private Anagram currentAnagram;
     private int score;
 
+    /**
+     * creates an instance of game
+     */
     private Game() {
-        // needs work
+        level = Difficulty.EASY;
+        anagrams = new HashMap<Difficulty, ArrayList<Anagram>>();
+        anagrams.put(Difficulty.EASY, FileReader.getAnagrams(Difficulty.EASY));
+        anagrams.put(Difficulty.MEDIUM, FileReader.getAnagrams(Difficulty.MEDIUM));
+        anagrams.put(Difficulty.HARD, FileReader.getAnagrams(Difficulty.HARD));
         rand = new Random();
     }
 
-    public static Game getInstance() { 
-        if (game == null) {
+    /**
+     * checks if game is null, if it is it calls Game() to create an instance
+     * @return game which is a Game
+     */
+    public static Game getInstance() {
+        if(game == null) {
             game = new Game();
         }
-    
+
         return game;
     }
-
+    
+    /**
+     * gets the question of the anagram dependent on the current score of the user
+     * @return currentAnagram.getQuestion() which is a String that stores question
+     * from the Anagram class
+     */
     public String getQuestion() {
-        // needs work
+        if (score >= 7) {
+            level = Difficulty.HARD;
+        } else if (score >= 4) {
+            level = Difficulty.MEDIUM;
+        } else {
+            level = Difficulty.EASY;
+        }
+
+        int size = anagrams.get(level).size() - 1;
+        currentAnagram = anagrams.get(level).get(rand.nextInt(size));
+
+        return currentAnagram.getQuestion();
     }
 
+    /**
+     * increments or decrements the score of the user based on if the user gave 
+     * a correct response and then returns if the user was correct
+     * @param userAnswer String
+     * @return currentAnagram.isCorrect(userAnswer) which is a boolean
+     */
     public boolean isCorrect(String userAnswer) {
-        // may need work
+        if (currentAnagram.isCorrect(userAnswer)) {
+            score++;
+        } else {
+            score--;
+        }
         return currentAnagram.isCorrect(userAnswer);
     }
 
+    /**
+     * gets the current score of the user
+     * @return score which is an int
+     */
     public int getScore() {
         return score;
     }
