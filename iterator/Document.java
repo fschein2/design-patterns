4bd059e8b4adabb602a781fd1a7cb93c68073c36
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-08 18:25:53 -0400

    completed iterator design-pattern

--- iterator/Document.java
+++ iterator/Document.java
@@ -2,41 +2,79 @@ package iterator;
 
 import java.util.Stack;
 
+/**
+ * Defines Document
+ * @author Fred Schein
+ */
 public class Document {
     private String title;
     private Stack<String> undoStack;
     private Stack<String> redoStack;
 
+    /**
+     * Creates an instance of a Document
+     * @param title which is a String
+     */
     public Document(String title) {
         this.title = title;
+        this.undoStack = new Stack<String>();
+        this.redoStack = new Stack<String>();
     }
 
+    /**
+     * makes a change to the document and adds it to the undoStack Stack
+     * @param change which is a String
+     * @return a String that the change was made
+     */
     public String makeChange(String change) {
+        undoStack.push(change);
         return "Making change: " + change;
     }
 
+    /**
+     * Undos the most recent change made and pushes it to the redoStack Stack
+     * @return a String that the change was undone
+     */
     public String undoChange() {
-
-        return "";
+        return "Undoing: " + redoStack.push(undoStack.pop());
     }
 
+    /**
+     * checks to see if there is anything in the undoStack Stack
+     * @return a boolean that is true if undoStack contains an item or false otherwise
+     */
     public boolean canUndo() {
-        return !(undoStack.isEmpty());
+        return undoStack.size() >= 1;
     }
 
-    public String redoChange() {
-        
-        return "";
+    /**
+     * Redos the most recent change that was undone and pushes it to the undoStack Stack
+     * @return a String that the change was redone
+     */
+    public String redoChange() {  
+        return "Redoing: " + undoStack.push(redoStack.pop());
     }
 
+    /**
+     * checks to see if there is anything in the redoStack Stack
+     * @return a boolean that is true if redoStack contains an item or false otherwise 
+     */
     public boolean canRedo() {
-        return !(redoStack.isEmpty());
+        return redoStack.size() >= 1;
     }
 
+    /**
+     * gets undoStack's Iterator
+     * @return a StackIterator that is associated with redoStack
+     */
     public StackIterator getUndoIterator() {
         return new StackIterator(undoStack);
     }
 
+    /**
+     * gets redoStack's Iterator
+     * @return a StackIterator that is associated with redoStack
+     */
     public StackIterator getRedoIterator() {
         return new StackIterator(redoStack);
     }
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -1,22 +1,38 @@
 package iterator;
 
 import java.util.Stack;
-import java.util.Iterator;
 
-public class StackIterator implements Iterator {
+/**
+ * defines StackIterator
+ * @author Fred Schein
+ */
+public class StackIterator {
     private Stack<String> items;
     private int position;
 
+    /**
+     * creates an instance of StackIterator
+     * @param items which is a Stack of Strings
+     */
     public StackIterator(Stack<String> items) {
         this.items = items;
     }
 
+    /**
+     * gets the next item in the Stack
+     * @return next which is a String of the next item in the Stack
+     */
     public String next() {
         String next = items.get(position);
         position++;
         return next;
     }
 
+    /**
+     * checks to see if the Stack has another item
+     * @return a boolean that is false if we are at a position that is greater than 
+     * the size or if that item is null otherwise it returns true
+     */
     public boolean hasNext() {
         if(position >= items.size() || items.get(position) == null) {
             return false;
